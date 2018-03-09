3e71bfba19090e466dfcf4fce05e531f231ca3f1
holmberd <holmberd@gmail.com>
2018-03-09 11:11:29 -0800

    Add equation 21 to help inform the choice of parameter p #issue 4

--- README.md
+++ README.md
@@ -1,22 +1,34 @@
 ## RBO - An implementation of Rank-Biased Overlap in JavaScript
 
-#### RBO: Object Constructor holding the RBO-state calculation_:
+#### RBO: Object Constructor holding the RBO-state calculation:
 
-```
+```javascript
 @constructor
-@param  {number} degree (0..1) of top-weightedness of the RBO metric
+@param {number} p - degree (0..1) of top-weightedness of the RBO metric
 ```
-
 Example: `var rbo = new RBO(0.8);`
 
-#### RBO.calculate: Calculates similarity RBO_:
+#### RBO.calculate: Calculates similarity RBO:
 
-```
-@param  {array, array} sorted ranked list arrays
+```javascript
+@function
+@param {Array} s -  sorted ranked list
+@param {Array} t - sorted ranked list
 @return {number} similarity RBO scores achieved
 ```
-
 Example: `rbo.calculate(['a', 'b', 'c', 'd', 'e'],['b', 'a', 'g', 'h', 'e', 'k', 'l', 'c']);`
+
+#### RBO.calcWeight: Calculates the weight of first d rankings with parameter p
+
+```javascript
+@function calcWeight
+@static
+@param {number} p - degree (0..1) of top-weightedness of the RBO metric
+@param {number} d - ranking
+```
+Example:
+`RBO.calcWeight(0.9, 10);`
+`helps inform the choice of the parameter p`
 
 ## License
 
--- rbo.js
+++ rbo.js
@@ -19,7 +19,7 @@
 /**
  * Object Constructor creates a new RBO state calculation
  * @constructor
- * @param  {number} degree (0..1) of top-weightedness of the RBO metric
+ * @param {number} p - degree (0..1) of top-weightedness of the RBO metric
  */
 var RBO = function (p) {
   this.p = p;
@@ -34,61 +34,69 @@ var RBO = function (p) {
 };
 
 /**
+ * Calculates the weight of first d rankings with parameter p
+ * @function calcWeight
+ * @static
+ * @param {number} p - degree (0..1) of top-weightedness of the RBO metric
+ * @param {number} d - ranking
+ */
+RBO.calcWeight = function(p, d) {
+  var summa = 0;
+  for (var i = 1; i < d; i++) {
+    summa += Math.pow(p, i) / i;
+  }
+  return 1 - Math.pow(p, (d - 1)) + ((1 - p) / p) * d * (Math.log( 1 / (1 - p) ) - summa);
+};
+
+/**
  * Calculates similarity RBO
  * @function calculate
- * @param  {array, array} sorted ranked list arrays
- * @return {function} extrapolated calculation
+ * @param {array} s - sorted ranked list
+ * @param {array} t - sorted ranked list
+ * @return {function} - extrapolated calculation
  */
 RBO.prototype.calculate = function (s, t) {
-
   if (t.length < s.length){
     var _t = s;
     s = t;
     t = _t;
   }
-
   for (var i = 0, l = s.length; i < l; i++){
     this.update(s[i], t[i]);
   }
-
   this.endShort();
-
   if (t.length > s.length){
     for (var n = s.length, le = t.length; n < le; n++){
       this.updateUneven(t[n]);
     }
   }
-
   return this.calcExtrapolated();
 };
 
 /**
  * Calculates the estimate beyond the original observation range
  * @function calcExtrapolated
- * @return {number} similarity RBO scores achieved
+ * @return {number} - similarity RBO scores achieved
  */
 RBO.prototype.calcExtrapolated = function () {
   var pl = Math.pow(this.p, this.depth);
-
   if (this.shortDepth == -1) {
     this.endShort();
   }
-
   return this.rbo + ((this.overlap-this.shortOverlap)/(this.depth)+((this.shortOverlap)/(this.shortDepth)))*pl;
 };
 
 /**
  * Adds elements from the two lists to our state calculation
  * @function RBO.prototype.update
- * @param  {element, element}
+ * @param {element} e1
+ * @param {element} e2
  */
 RBO.prototype.update = function (e1, e2) {
-
   if (this.shortDepth != -1){
     console.log("RBO: update() called after EndShort()");
     return false;
   }
-
   if (e1 == e2){
     this.overlap++;
   }
@@ -109,7 +117,6 @@ RBO.prototype.update = function (e1, e2) {
       this.seen.set(e2, true);
     }
   }
-
   this.depth++;
   this.wgt *= this.p;
   this.rbo += (this.overlap / this.depth) * this.wgt;
@@ -127,26 +134,21 @@ RBO.prototype.endShort = function () {
 /**
  * Adds elements from the longer list to the state calculation
  * @function UpdateUneven
- * @param  {element}
+ * @param {element}
  */
 RBO.prototype.updateUneven = function (e) {
-
   if (this.shortDepth == -1) {
     console.log("RBO: UpdateUneven() called without EndShort()");
     return false;
   }
-
   if (this.seen[e]) {
     this.overlap++;
     this.seen[e] = false;
   }
-
   this.depth++;
   this.wgt *= this.p;
-
   this.rbo += (this.overlap / this.depth) * this.wgt;
   this.rbo += ((this.shortOverlap*(this.depth-this.shortDepth)) / (this.depth*this.shortDepth)) * this.wgt;
-
 };
 
 module.exports = RBO;
